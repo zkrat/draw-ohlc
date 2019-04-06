bbb12c9a2d222f7bf67aef5f11b3caca9f72d76f
zkrat <hajek@zkrat.net>
2019-04-06 20:39:00 +0200

    OhlcLengthHelper added

---
+++ src/Helper/OhlcLengthHelper.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zkrat
+ * Date: 06.04.19
+ * Time: 20:27
+ */
+
+namespace DrawOHLC\Helper;
+
+
+class OhlcLengthHelper {
+
+	const MINUTE=60;
+	const HOUR=3600;
+	const DAY=86400;
+	const WEEK=604800;
+
+	public static function getStrintg(int $seconds ) {
+		if ($seconds<OhlcLengthHelper::MINUTE)
+			return $seconds.'s';
+		elseif ($seconds<OhlcLengthHelper::HOUR){
+			return round($seconds/OhlcLengthHelper::MINUTE).'m';
+		}
+		elseif ($seconds<OhlcLengthHelper::DAY){
+			return round($seconds/OhlcLengthHelper::HOUR).'D';
+		}
+		elseif ($seconds<OhlcLengthHelper::WEEK){
+			return round($seconds/OhlcLengthHelper::DAY).'D';
+		}
+
+		return '~?';
+	}
+
+}
